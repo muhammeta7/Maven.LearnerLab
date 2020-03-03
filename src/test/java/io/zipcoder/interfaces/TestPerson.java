4c5088fa3e18375e81843cfa9980b17790ad992f
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-03-03 18:43:34 -0500

    complete Person class and tests

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,43 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
 
+    @Test
+    public void TestConstructor(){
+
+        // Given
+        long id = 128282829;
+        String name = "Moe";
+        Person person = new Person(id, name);
+
+        // When
+        long expectedId = person.getId();
+        String expectedName = person.getName();
+
+
+        // Then
+        Assert.assertEquals(id, expectedId);
+        Assert.assertEquals(name, expectedName);
+
+    }
+
+    @Test
+    public void TestSetName(){
+
+        // Given
+        Person person = new Person(102929, null);
+        String expected = "Moe";
+        person.setName(expected);
+
+        // When
+        String actual = person.getName();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+        
+    }
 }
+
